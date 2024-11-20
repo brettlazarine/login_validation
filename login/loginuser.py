92c90ac1fc9fa0be5f32d011ea626c514a0d041d
brettlazarine <jblazarine@gmail.com>
2024-11-20 10:58:19 -0600

    create login and checks

---
+++ login/loginuser.py
@@ -0,0 +1,12 @@
+from database.db import (read_users)
+
+def login_user(email, password):
+    query = "SELECT * FROM users WHERE email = ?"
+    res = read_users(query, email)
+    if not res:
+        return (False, "Email not found, please try again or register")
+    pw = res[0][2]
+    print(pw)
+    if password != pw:
+        return (False, "Incorrect password")
+    return (True, "Login successful")
